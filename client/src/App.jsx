3e8b8eb31e9ef85c133ff45c45c94cfa9b7db8fd
Arda-f <96407570+Arda-f@users.noreply.github.com>
2023-01-03 15:46:26 +0000

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header"
 import News from "./components/news"
+import Learn from "./components/learn"
 import "./css/main.css"
 import "./css/news.css"
 import "./css/header.css"
@@ -8,8 +9,9 @@ import "./css/learn.css"
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <News/>
+      <Header />
+      <News />
+      <Learn/>
     </div>
   );
 }
---
+++ client/src/components/learn.jsx
@@ -0,0 +1,12 @@
+import rct from "../icons/react.svg"
+export default function learn(){
+    return(
+        <div className="learn-line">
+            <div className="learn-container">
+                <div className="icon">
+                    <img src={rct} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
